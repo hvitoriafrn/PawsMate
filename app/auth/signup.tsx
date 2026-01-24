547f12c65a440ed308325321577da27564cf46e5
Vitoria <hvitoriaalmeida@gmail.com>
2026-01-24 20:10:52 +0000

    Updated the sign up page to work with the Firebase functions

--- app/auth/signup.tsx
+++ app/auth/signup.tsx
@@ -1,5 +1,6 @@
 // Import the necessary libraries and components 
 import { auth } from '@/config/firebase';
+import { createUserDocument } from '@/services/firebase/firestoreService';
 import { useUserStore } from '@/store/userStore';
 import { useRouter } from 'expo-router';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
@@ -9,54 +10,104 @@ import {
   ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View
 } from 'react-native';
 
+
 // Sign Up Screen Component
 export default function SignUpScreen() {
   const router = useRouter(); // Router for navigation
   // Get the functions from Zustand store
-  const { setUser, setLoading, setError } = useUserStore();
+  const { setUser, setError } = useUserStore();
   
   // Get the necessary information from the user 
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
+  const [age, setAge] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [location, setLocation] = useState('');
+  const [bio, setBio] = useState('');
+  const [loading, setLoading] = useState(false);
 
   //  Handle user sign up when the sign up button is pressed
   const handleSignUp = async () => {
     // Validation to check if all the fields are filled
-    if (!name || !email || !password || !confirmPassword) {
-      Alert.alert('Error', 'Please fill in all fields'); // if a field is empty, error
+    if (!name.trim()) {
+        Alert.alert('Error', 'Please enter your name');
+        return;
+    }
+
+    if (!age || isNaN(parseInt(age)) || parseInt(age) < 18) { 
+        Alert.alert('Error','You must be at least 18 years old');
+        return;
+    }
+
+    if (!location.trim()) {
+        Alert.alert('Error','Please enter your location');
+        return;
+    }
+
+    if (!email.trim() || !password.trim()) {
+      Alert.alert('Error','Please fill in all fields');
       return;
     }
-    // Password validation for both entered passwords to ensure they match
-    if (password !== confirmPassword) {
-      Alert.alert('Error', 'Passwords do not match');
-      return;
-    }
-    // Check if password meets minimum character requirement
+
     if (password.length < 8) {
-      Alert.alert('Error', 'Password must contain at least 8 characters');
+      Alert.alert('Error','Password must be at least 8 characters');
       return;
     }
-    
+  
     // Start loading state
     setLoading(true);
 
     // If all the validation passed, try to create account
     try {
       // Sends user's details (email and password to Firebase)
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-      setUser(userCredential.user); 
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email, 
+        password
+      ); 
+      const user = userCredential.user
+      
+      // Log to the console the acct has been created
+      console.log('User acct created: ', user.uid);
+
+
+      await createUserDocument (
+        user.uid,
+        email,
+        name,
+        parseInt(age),
+        location, 
+        bio || undefined
+      );
+
+      // Log to the console
+      console.log('Firestore doc created');
+
+      setUser({
+        uid: user.uid,
+        email: user.email || '',
+        displayName: name,
+      });
+
       // if all correct, create user, show a message to user to let them know
       Alert.alert('Success', 'Account created successfully!');
-      
       // Navigate to main app
-      router.replace('/(tabs)'); // because of 'replace' the user can't go to signup scren
+      router.replace('/(tabs)'); // because of 'replace' the user can't go to signup screen
       
       // If any errors occuer, show the error to the user
     } catch (error: any) {
       console.error('Signup error:', error);
-      setError(error.message);
+
+      let errorMessage = error.message;
+
+      if (error.code === 'auth/email-already-in-use') {
+          errorMessage = 'An account linked to this email already exists. Please use login.'
+      } else if (error.code === 'auth/invalid-email') {
+          errorMessage = 'Invalid email adress format. Please enter a valid email address.'
+      } else if (error.code === 'auth/weak-password') {
+          errorMessage = 'Password does not meet the requirements. Use at least 8 characters.'
+      }
       Alert.alert('Sign Up Failed', error.message);
 
       // Stops loading
@@ -116,6 +167,34 @@ export default function SignUpScreen() {
               onChangeText={setConfirmPassword}
               secureTextEntry
               autoComplete="password"
+            />
+
+              {/* Age input */}
+            <TextInput
+              style={styles.input}
+              placeholder="Age"
+              value={age}
+              onChangeText={setAge}
+              keyboardType="numeric"
+            />
+
+              {/* Location input */}
+            <TextInput
+              style={styles.input}
+              placeholder="Planet Earth"
+              value={location}
+              onChangeText={setLocation}
+              autoCapitalize="words"
+            />
+
+              {/* Bio input */}
+            <TextInput
+              style={styles.input}
+              placeholder="It's me, hi, I'm the entirely uninteresting bio."
+              value={bio}
+              onChangeText={setBio}
+              multiline
+              numberOfLines={3}
             />
 
             {/* Sign up button*/}
