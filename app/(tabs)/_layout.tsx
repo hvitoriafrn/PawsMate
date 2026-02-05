d5ce232a76d582238f9fc6fa79a9d923a3b2a17e
Vitoria <hvitoriaalmeida@gmail.com>
2026-02-05 04:44:53 +0000

    update for eas
    Small changes to try and work with EAS

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,56 @@
-import { Tabs } from "expo-router";
+// import necessary modules and components
+import { HapticTab } from '@/components/haptic-tab';
+import { IconSymbol } from '@/components/ui/icon-symbol';
+import { Colors } from '@/constants/theme';
+import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Tabs } from 'expo-router';
+import React from 'react';
 
+// defines the layout for the tab navigation
 export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  // Render the tab navigator with tabs for home and explore
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
-      <Tabs.Screen name="explore" options={{ title: "Explore" }} />
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+      }}>
+
+      {/* Home tab*/}
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+        }}
+      />
+
+      {/* Explore tab*/}
+      <Tabs.Screen
+        name="explore"
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+        }}
+      />
+
+      <Tabs.Screen
+       name="events" 
+       options={{ 
+        href: null 
+        }} 
+        />
+
+      <Tabs.Screen
+       name="messages" 
+       options={{ 
+        href: null 
+        }} 
+        />
+
     </Tabs>
   );
 }
