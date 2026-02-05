3e73f903ea808e9a73238a76b04267ec3b04a8f5
Vitoria <hvitoriaalmeida@gmail.com>
2026-02-05 04:05:10 +0000

    Use minimal tabs for EAS, trying for a stable publish

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,41 +1,10 @@
-// import necessary modules and components
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
-import { Tabs } from 'expo-router';
-import React from 'react';
+import { Tabs } from "expo-router";
 
-// defines the layout for the tab navigation
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
-  // Render the tab navigator with tabs for home and explore
   return (
-    <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-      }}>
-
-      {/* Home tab*/}
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-
-      {/* Explore tab*/}
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+    <Tabs screenOptions={{ headerShown: false }}>
+      <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen name="explore" options={{ title: "Explore" }} />
     </Tabs>
   );
 }
